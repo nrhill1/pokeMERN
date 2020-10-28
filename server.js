47c54d9df7fdca7e3379c56d8f03a9cbf9617871
Nicolas Hill <nrhill1@gmail.com>
2020-10-27 21:48:45 -0700

    server operational

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 
 // Connect to Mongo
-mongoose.connect(db)
+mongoose
+    .connect(db)
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
