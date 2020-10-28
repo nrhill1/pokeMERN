4f502eb9172e96b5e3000586b26f8f428c475760
Nicolas Hill <nrhill1@gmail.com>
2020-10-27 21:26:29 -0700

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://nic:123@cluster0.5ojcu.mongodb.net/pokemern?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose.connect(db)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
