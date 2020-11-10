96c3381f43f31247f9a38c28d1e228425d4b48c1
Nicolas Hill <nrhill1@gmail.com>
2020-11-09 17:56:38 -0800

    renamed loginReducer file

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import loginReducer from './isLogged.js';
+import loginReducer from './loginReducer.js';
 
 export default combineReducers({
     login: loginReducer
