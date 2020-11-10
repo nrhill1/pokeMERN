1bd4719298bf73016b33365f05961959d4821643
Nicolas Hill <nrhill1@gmail.com>
2020-11-09 19:26:04 -0800

    made profile auth dependent

--- frontend/src/Components/Navigation.js
+++ frontend/src/Components/Navigation.js
@@ -8,11 +8,11 @@ class Navigation extends Component {
     return(
       <div>
         <Navbar bg="dark" variant="dark" className="navbar">
-          ><Nav.Link className="navLink" href="/">Home</Nav.Link>
-          ><Nav.Link className="navLink" href="/user">Profile</Nav.Link>
-          ><Nav.Link className="navLink" href="/login">Login</Nav.Link>
-          ><Nav.Link className="navLink" href="/register">Register</Nav.Link>
-          ><Nav.Link className="navLink">Logout</Nav.Link>
+          <Nav.Link className="navLink" href="/">Home</Nav.Link>
+          { localStorage.token ? <Nav.Link className="navLink" href="/user">Profile</Nav.Link>: ""}
+          <Nav.Link className="navLink" href="/login">Login</Nav.Link>
+          <Nav.Link className="navLink" href="/register">Register</Nav.Link>
+          <Nav.Link className="navLink">Logout</Nav.Link>
         </Navbar>
       </div>
     )
