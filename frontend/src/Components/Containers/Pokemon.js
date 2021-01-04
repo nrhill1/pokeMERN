e23d4afd89c98f236c2ab56e4bbd1acffacea7f5
Nicolas Hill <nrhill1@gmail.com>
2021-01-03 16:42:19 -0800

    made some cool hover effects for pokemon cards

--- frontend/src/Components/Containers/Pokemon.js
+++ frontend/src/Components/Containers/Pokemon.js
@@ -1,94 +1,94 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Alert, Card, Button } from "react-bootstrap";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Alert, Card, Button } from 'react-bootstrap';
 
 String.prototype.capitalize = function() {
-  return this.charAt(0).toUpperCase() + this.slice(1);
+	return this.charAt(0).toUpperCase() + this.slice(1);
 };
 
 class Pokemon extends Component {
-  state = {
-    msg: null
-  };
+	state = {
+		msg: null
+	};
 
-  componentDidUpdate(prevProps) {
-    const { errorReducer } = this.props;
-    if (errorReducer !== prevProps.errorReducer) {
-      // Check for delete error
-      if (errorReducer.id === "RELEASE_FAIL") {
-        this.setState({ msg: errorReducer.msg });
-      } else {
-        this.setState({ msg: null });
-      }
-    }
-  }
+	componentDidUpdate(prevProps) {
+		const { errorReducer } = this.props;
+		if (errorReducer !== prevProps.errorReducer) {
+			// Check for delete error
+			if (errorReducer.id === 'RELEASE_FAIL') {
+				this.setState({ msg: errorReducer.msg });
+			} else {
+				this.setState({ msg: null });
+			}
+		}
+	}
 
-  render() {
-    // const { isAuth, user } = this.props.authReducer;
-    return (
-      <div className="pokemon">
-        {this.state.msg ? (
-          <Alert color="danger">{this.state.msg.msg}</Alert>
-        ) : null}
-        <Card
-          border="dark"
-          style={{
-            maxHeight: "fit-content",
-            maxWidth: "fit-content",
-            margin: "6px",
-            display: "inline-block"
-          }}
-        >
-          <Card.Img
-            variant="top"
-            src={this.props.pokemon.sprites[0]}
-            style={{
-              maxWidth: "8rem",
-              maxHeight: "8rem",
-              margin: "0 auto",
-              display: "inline-block"
-            }}
-          />
-          <Card.Img
-            variant="top"
-            src={this.props.pokemon.sprites[1]}
-            style={{
-              maxWidth: "8rem",
-              maxHeight: "8rem",
-              margin: "0 auto",
-              display: "inline-block"
-            }}
-          />
-          <Card.Body>
-            <Card.Title
-              style={{
-                margin: "0 auto"
-              }}
-            >
-              #{this.props.pokemon.id} {this.props.pokemon.name.capitalize()}
-            </Card.Title>
-          </Card.Body>
-          <Button
-            variant="danger"
-            onClick={this.props.onDelete}
-            style={{
-              marginBottom: "4px",
-              maxWidth: "200px",
-              maxHeight: "70px",
-              display: "inline-block"
-            }}
-          >
-            Remove from team
-          </Button>
-        </Card>
-      </div>
-    );
-  }
+	render() {
+		// const { isAuth, user } = this.props.authReducer;
+		return (
+			<div className="pokemon">
+				{this.state.msg ? <Alert color="danger">{this.state.msg.msg}</Alert> : null}
+				<Card
+					className="pokemonCard"
+					border="dark"
+					style={{
+						maxHeight: 'fit-content',
+						maxWidth: 'fit-content',
+						margin: '6px',
+						display: 'inline-block'
+					}}
+				>
+					<Card.Img
+						variant="top"
+						src={this.props.pokemon.sprites[0]}
+						style={{
+							maxWidth: '8rem',
+							maxHeight: '8rem',
+							margin: '0 auto',
+							display: 'inline-block'
+						}}
+					/>
+					<Card.Img
+						variant="top"
+						src={this.props.pokemon.sprites[1]}
+						style={{
+							maxWidth: '8rem',
+							maxHeight: '8rem',
+							margin: '0 auto',
+							display: 'inline-block'
+						}}
+					/>
+					<Card.Body>
+						<Card.Title
+							style={{
+								margin: '0 auto'
+							}}
+						>
+							#{this.props.pokemon.id} {this.props.pokemon.name.capitalize()}
+						</Card.Title>
+					</Card.Body>
+					<Button
+						className="removeButton"
+						variant="danger"
+						onClick={this.props.onDelete}
+						style={{
+							marginBottom: '4px',
+							maxWidth: '200px',
+							maxHeight: '70px',
+							display: 'inline-block'
+						}}
+					>
+						Remove from team
+					</Button>
+				</Card>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = (state) => ({
-  authReducer: state.authReducer,
-  errorReducer: state.errorReducer
+	authReducer: state.authReducer,
+	errorReducer: state.errorReducer
 });
 
 /*
